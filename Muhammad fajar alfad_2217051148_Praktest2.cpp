3f09f209048ead7d74dc1a1e15dc074397b4b760
muhammad fajar alfad <112568404+fajaralfad@users.noreply.github.com>
2022-09-22 12:54:31 +0700

    Add files via upload

---
+++ Muhammad fajar alfad_2217051148_Praktest2.cpp
@@ -0,0 +1,27 @@
+/*nama saya muhammad fajar alfad
+  dengan nomor npm 2217051148
+  kelas c*/
+  
+#include <iostream>
+using namespace std;
+
+int main (){
+	
+	int angka;
+	cout <<"masukan angka :";
+	cin >> angka;
+	
+	if (angka == 1){
+		cout <<"satu";
+	}
+	else if (angka == 2){
+		cout <<"dua";
+	}
+	else if (angka == 3){
+		cout <<"tiga";
+	}
+	else if (angka >= 3){
+		cout <<"bilangan lebih besar dari tiga";
+	}
+	return 0;
+}
