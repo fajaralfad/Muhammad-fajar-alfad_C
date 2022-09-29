b6fdd7d9a13e45d8c320ad6162e0a09125de2f81
muhammad fajar alfad <112568404+fajaralfad@users.noreply.github.com>
2022-09-29 22:49:19 +0700

    Add files via upload

---
+++ 2217051148_muhammad fajar alfad_praktest3.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main ()
+{
+	int a,b,c;
+	
+	cout <<"masukan angka pertama :";
+	cin >> a;
+	
+	cout <<"masukan angka kedua   :";
+	cin >> b;
+	
+	for (c = 0; c <= b; c+=a){
+		
+		cout <<c <<endl;
+        }
+return 0;
+}
